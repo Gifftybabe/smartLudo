85b304c1d483a12d1ddea57125c56a0ecfff6b79
Gifftybabe <ulokangozi@gmail.com>
2024-10-03 10:16:49 +0100

    deployment update

--- ignition/modules/Ludo.ts
+++
@@ -1,9 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("SmartLudoModule", (m) => {
-
-
-  const Ludo = m.contract("SmartLudo");
-
-  return { Ludo };
-});
